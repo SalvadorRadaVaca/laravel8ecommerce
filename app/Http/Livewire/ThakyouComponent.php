3496d8e964e28082edfaca2b3351eb4c83e820f7
Sal565 <radavacasalvador@gmail.com>
2021-07-26 11:57:34 -0500

    upload files

---
+++ app/Http/Livewire/ThakyouComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ThakyouComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.thakyou-component')->layout('layouts.base');
+    }
+}
